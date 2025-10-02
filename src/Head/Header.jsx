3bdaf7ea9bcd81779dd23c748a559fa520c607f9
Yaman Kumar Yadav <143440408+YamanYadav01@users.noreply.github.com>
2025-10-02 22:37:31 +0530

    Update Header.jsx
    commit for responsive

--- src/Head/Header.jsx
+++ src/Head/Header.jsx
@@ -3,8 +3,27 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './header.css'
+import { useNavigate } from 'react-router-dom';
+import { TbLogout2 } from "react-icons/tb";
 
-function Header({selectedFriend}) {
+function Header({selectedFriend,IsOpen,isOpen}) {
+
+  const Navigate = useNavigate();
+
+  // const toggleBtn = document.querySelector(".manubar");
+  // toggleBtn.addEventListener("click", () => {
+  // sidebar.classList.toggle("active");
+  // });
+  
+  const handleClick = () => {
+    // sidebar.classList.toggle("active");
+    if(isOpen==false){
+      IsOpen(true);
+    }
+    else{
+      IsOpen(false);
+    }
+  }
   
   return ( 
     <>
@@ -15,10 +34,11 @@ function Header({selectedFriend}) {
           <div className='chatUser'>
           {selectedFriend ? (
                 <div className='messageHeader'>
+                     <span className='manubar' onClick={handleClick}>☰</span>&nbsp;
                     <div className='circle'></div><span className='username' id={selectedFriend._id}>{selectedFriend.username}</span>
                 </div>)
                :(
-        <h2>Select a friend to start chatting</h2>
+        <h2><span className='manubar"' onClick={handleClick}>☰</span>&nbsp;Select a friend to start chatting</h2>
       )
           }
           </div>
