5bbd1574bb0de8f7d0bff243412916f1ef88d8f7
Yaman Kumar Yadav <143440408+YamanYadav01@users.noreply.github.com>
2026-04-19 23:45:51 +0530

    Remove commented out withCredentials option

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,6 @@ import { io } from "socket.io-client";
 
 const socket = io("https://chatapp-backend-2-s1pn.onrender.com", {
   transports: ["polling", "websocket"]
-  // ❌ withCredentials: true REMOVE karo
 });
 
-export default socket;
+export default socket; 
