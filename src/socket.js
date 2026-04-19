a1805e6821cbdcb561f2876985ef9a4ac549021c
Yaman Kumar Yadav <143440408+YamanYadav01@users.noreply.github.com>
2026-04-19 23:37:17 +0530

    Change socket transports to include polling

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const socket = io("https://chatapp-backend-2-s1pn.onrender.com", {
-  transports: ["websocket"],   // ✅ correct
+  transports: ["polling", "websocket"],
   reconnection: true,
   reconnectionAttempts: 5
 });
