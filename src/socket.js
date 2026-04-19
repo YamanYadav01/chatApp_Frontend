060c9da78cdcf5536a990f4e36cea229ffd36ae4
Yaman Kumar Yadav <143440408+YamanYadav01@users.noreply.github.com>
2026-04-19 21:13:09 +0530

    Add reconnection options to socket connection

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,10 @@
 import { io } from "socket.io-client";
 
 const socket = io("https://chatapp-backend-2-s1pn.onrender.com", {
-  transports: ["websocket"],   // 🔥 MOST IMPORTANT
-  withCredentials: true
+  transports: ["websocket"],   // ✅ correct
+  withCredentials: true,
+  reconnection: true,
+  reconnectionAttempts: 5
 });
 
 export default socket; 
