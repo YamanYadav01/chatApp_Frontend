2224ad5bfbf46db071b874c625a3e8453f58a2ee
Yaman Kumar Yadav <143440408+YamanYadav01@users.noreply.github.com>
2026-04-19 21:03:38 +0530

    Update socket connection with options

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,8 @@
-import io from 'socket.io-client'
+import { io } from "socket.io-client";
 
-const socket = io('https://chatapp-backend-2-s1pn.onrender.com/');
+const socket = io("https://chatapp-backend-2-s1pn.onrender.com", {
+  transports: ["websocket"],   // 🔥 MOST IMPORTANT
+  withCredentials: true
+});
 
 export default socket; 
