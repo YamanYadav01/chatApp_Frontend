e7230969d1b9e234578ef581a9396223b97e43b0
Yaman Kumar Yadav <143440408+YamanYadav01@users.noreply.github.com>
2026-04-19 23:35:47 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,6 @@ import { io } from "socket.io-client";
 
 const socket = io("https://chatapp-backend-2-s1pn.onrender.com", {
   transports: ["websocket"],   // ✅ correct
-  withCredentials: true,
   reconnection: true,
   reconnectionAttempts: 5
 });
