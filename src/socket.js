86013359493e85d256ea7f4d270e1e3a1286c5e3
Yaman Kumar Yadav <143440408+YamanYadav01@users.noreply.github.com>
2026-04-28 00:12:04 +0530

    Add withCredentials to socket connection options
    Updated socket connection options to include withCredentials.

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const socket = io("https://chatapp-backend-2-s1pn.onrender.com", {
-  transports: ["polling", "websocket"]
+   withCredentials: true
 });
 
 export default socket; 
