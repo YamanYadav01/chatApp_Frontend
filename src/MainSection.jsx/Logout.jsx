6cbdcdb15e1b197b1db4d9831600a8b3e54c71eb
yamankumaryadav2001@gmail.com <yamankumaryadav2001@gmail.com>
2026-07-21 11:38:17 +0530

    add state contactlist

--- src/MainSection.jsx/Logout.jsx
+++ src/MainSection.jsx/Logout.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import cookie from 'js-cookie'
 import Cookies from 'js-cookie';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import { ToastContainer,toast } from 'react-toastify';
 
 function Logout() {
